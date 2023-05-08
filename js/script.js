3ac85e1fb8e35a586498c3239ea1117552728e00
Luiz Tavares <luizcarius70@gmail.com>
2023-05-08 02:16:18 -0300

    tooltip transformado em classe

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
+import ToolTip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initDateObject from "./modules/date-object.js";
@@ -29,7 +29,9 @@ const modal = new Modal(
 );
 modal.init();
 
-initTooltip();
+const toolTip = new ToolTip("[data-tooltip]");
+toolTip.init();
+
 initDropdownMenu();
 initMenuMobile();
 initDateObject();
