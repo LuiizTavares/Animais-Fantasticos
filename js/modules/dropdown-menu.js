91c63d2033b46b67517e4beecf45009fbef7ff3c
Luiz Tavares <luizcarius70@gmail.com>
2023-05-10 00:16:39 -0300

    dropdown menu refatorado

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,39 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+export default class DropdownMenu {
+  constructor(menus, events) {
+    this.dropdownMenus = document.querySelectorAll(menus);
 
-  dropdownMenus.forEach((item) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      item.addEventListener(userEvent, handleClick);
-    });
-  });
+    // define touchstart e click argumento padrao
+    // de events caso o usuario nao defina
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
+    this.activeClass = "active";
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
 
-  function handleClick(event) {
+  // ativa o dropdown menu e adiciona a função
+  // que observa o clique fora dele
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add("active");
-    outsideClick(this, ["touchstart", "click"], () => {
-      this.classList.remove("active");
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
+  }
+
+  // adiciona os eventos ao dropdownmenu
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((item) => {
+      this.events.forEach((userEvent) => {
+        item.addEventListener(userEvent, this.activeDropdownMenu);
+      });
+    });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) this.addDropdownMenusEvent();
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
 import ToolTip from "./modules/tooltip.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
+import DropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initDateObject from "./modules/date-object.js";
 import fetchAnimais from "./modules/fetch-animais.js";
@@ -35,7 +35,9 @@ toolTip.init();
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
-initDropdownMenu();
+const dropdownMenu = new DropdownMenu("[data-dropdown]");
+dropdownMenu.init();
+
 initMenuMobile();
 initDateObject();
 
